a6850bf0710c53fbbb5ebd3eadb3ff1c5ac264ed
MusicJunkyREZ <reidzuckerman@gmail.com>
2020-04-26 16:37:49 -0600

    edited deploy

---
+++ build/precache-manifest.bf86e15f5b257e112e88bf2902125027.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c28e0eecda9721c4103be505189d041",
+    "url": "/casosdecoronavirus/index.html"
+  },
+  {
+    "revision": "7bd9a483b799128791b4",
+    "url": "/casosdecoronavirus/static/css/main.12c7c84a.chunk.css"
+  },
+  {
+    "revision": "6c133beac76589122c2a",
+    "url": "/casosdecoronavirus/static/js/2.73732098.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/casosdecoronavirus/static/js/2.73732098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd9a483b799128791b4",
+    "url": "/casosdecoronavirus/static/js/main.8ded2440.chunk.js"
+  },
+  {
+    "revision": "7715db9912b9a3c853ac",
+    "url": "/casosdecoronavirus/static/js/runtime-main.8f87101a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/casosdecoronavirus/static/media/virus.d7265326.png"
+  }
+]);
